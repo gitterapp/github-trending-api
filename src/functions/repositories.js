200df32923aaa3a48f3e526154d81d7504ad8fda
kjxbyz <kjxbyz888@outlook.com>
2022-09-30 19:30:59 +0800

    fix: update repositories

--- src/functions/repositories.js
+++ src/functions/repositories.js
@@ -3,7 +3,7 @@ const { success, error } = require('./utils/response');
 
 exports.handler = async ({ queryStringParameters: params }) => {
   try {
-    const { language, since, spoken_language_code: spokenLanguage } = params;
+    const { language, since, spoken_language: spokenLanguage } = params;
     const data = await fetchRepositories({ language, since, spokenLanguage });
     if (data && data.length > 0) {
       return success(data);
